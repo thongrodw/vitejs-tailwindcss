c64bf7d27c5c757bebc620781800b21e26571f7d
Wongsatorn Thongrod <43429376+thongrodw@users.noreply.github.com>
2022-11-25 14:14:34 +0000

    Add Components and Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+import {close,menu,logo} from '../assets';
+import {navLinks} from '../constants';
+
+const Navbar = () => {
+  return (
+    <div className="w-full flex py-6 justify-between items-center navbar">Navbar</div>
+  )
+}
+
+export default Navbar
